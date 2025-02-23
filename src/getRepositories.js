63b5bdee48b19e618f3163e979506bd84644ab3b
Johnson <jnghana@gmail.com>
2025-02-23 08:56:12 -0700

    move from other repo to this project

---
+++ src/getRepositories.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const { getRepositories: getRepos } = require('./utils');
+
+function hasFlag(flag, flags, defaultValue = false) {
+	return !!(flags?.length > 0 ? flags?.includes(flag) : defaultValue);
+}
+
+function generateQuery(endCursor, { f }, perPage = 20) {
+	const showForks = hasFlag('forks', f);
+	const showSources = hasFlag('sources', f, true);
+	const showPrivate = hasFlag('private', f);
+	const showPublic = hasFlag('public', f, true);
+
+	return `
+		query {
+			viewer {
+				repositories(
+					first: ${perPage}
+					affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
+					${endCursor ? `after: "${endCursor}"` : ''}
+					${showForks === showSources ? '' : `isFork: ${!!showForks}`}
+					${showPrivate === showPublic ? '' : `privacy: ${showPublic ? 'PUBLIC' : 'PRIVATE'}`}
+				) {
+					totalCount
+					pageInfo {
+						endCursor
+						hasNextPage
+					}
+					nodes {
+						name
+						nameWithOwner
+						defaultBranchRef {
+							name
+							branchProtectionRule {
+								allowsForcePushes
+								allowsDeletions
+								dismissesStaleReviews
+								requiredApprovingReviewCount
+								requiresApprovingReviews
+								requiresCodeOwnerReviews
+								requiresConversationResolution
+								restrictsPushes
+								requiredStatusChecks {
+									app {
+										id
+									}
+								}
+							}
+						}
+						deleteBranchOnMerge
+						hasIssuesEnabled
+						hasProjectsEnabled
+						hasDiscussionsEnabled
+						hasWikiEnabled
+						webCommitSignoffRequired
+						forkingAllowed
+						isArchived
+						autoMergeAllowed
+						squashMergeCommitTitle
+						isBlankIssuesEnabled
+						isFork
+						isPrivate
+						isSecurityPolicyEnabled
+						codeOfConduct {
+							name
+						}
+						isTemplate
+						licenseInfo {
+							name
+						}
+						mergeCommitAllowed
+						owner {
+							login
+						}
+						rebaseMergeAllowed
+						squashMergeAllowed
+						createdAt
+						updatedAt
+						pushedAt
+						viewerHasStarred
+						viewerPermission
+						viewerSubscription
+					}
+				}
+			}
+			rateLimit {
+				cost
+				remaining
+			}
+		}
+	`;
+}
+
+module.exports = async function getRepositories(flags, filter) {
+	// Get all repositories
+	const { points, repositories } = await getRepos(generateQuery, flags, { filter });
+
+	if (!flags.sort) {
+		repositories.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+	}
+
+	return { points, repositories };
+};
